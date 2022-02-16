5e19090de8597c9dcd6e1a903dc655bbc17fbbdf
juancarloscr17 <juankcr17@gmail.com>
2022-02-15 22:43:44 -0500

    Operador Aritmetico Exponenciacion

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-// Operador Aritmetico Multiplicacion
+// Operador Aritmetico Exponenciacion
 let num1 = parseInt(prompt(`Ingrese el primer numero`, 1));
 let num2 = new Number(prompt(`Ingrese el segundo numero`,2));
 let myStyle = `
-    color: 	#fff;
-    background: #006400;
+    color: 	#000;
+    background: #ff8c00;
     padding: 5px;
     border-radius: 5px;
 `;
-console.group(`%cOperador Aritmetico Multiplicacion`, myStyle);
+console.group(`%cOperador Aritmetico Exponenciacion`, myStyle);
 
  // Significado
-    let resultado = num1 * num2;
-    console.log(`Numero 1 X Numero 2 : ${num1} X ${num2} =${resultado}`);
-    console.log(`La Multiplicacion de los numeros es: ${resultado}`);
+    let resultado = num1 ** num2;
+    console.log(`Numero 1 elevado al Numero 2 : ${num1} ** ${num2} =${resultado}`);
+    console.log(`${num1} elevado al ${num2}: ${resultado}`);
 
 // Abreviacion
-    console.log(`Abreviacion: ${num1} *= ${num2}`)
-    num1*=num2
-    console.log(`La Multiplicacion es : ${num1}`);
+    console.log(`Abreviacion: ${num1} **= ${num2}`)
+    num1**=num2
+    console.log(`El resultado del exponencia es : ${num1}`);
     console.groupEnd();
 
 
