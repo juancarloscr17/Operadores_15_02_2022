562e67662bb316e308f70984bd4f685b06162912
juancarloscr17 <juankcr17@gmail.com>
2022-02-15 21:56:34 -0500

    Operador Aritmetico Suma

--- main.js
+++ main.js
@@ -1,64 +1,25 @@
-// Operadores de Adicion
-let x = 25;
-let y = 10;
+// Operador Aritmetico Suma
+let num1 = parseInt(prompt(`Ingrese el primer numero`, 1));
+let num2 = new Number(prompt(`Ingrese el segundo numero`,2));
 let myStyle = `
     color: #000;
-    background: #7FFF00;
+    background: #00ffff;
     padding: 5px;
     border-radius: 5px;
 `;
 
 // Adición
-console.group(`%cOperadores Adición`, myStyle);
+console.group(`%cOperadores Aritmetico Suma`, myStyle);
 
+ // Significado
+   
+    let resultado = num1 + num2;
+    console.log(`Numero 1: ${num1} + Numero 2: ${num2}`);
+    console.log(`La suma de los numeros es: ${resultado}`);
 
-    console.log(`Variable x: ${x} Variable y: ${y}`);
-
-    // Abreviacion
-    console.log(`Abreviacion: x += y;`);
-    x += y;
-    console.log(`Resultado : x : ${x}`)
-    // // Significado
-    // console.log(`Significado: x = x + y;`);
-    // x = x + y;
-
-    // console.log(`x: ${x} y: ${y}`);
-    // console.log(`Resultado : x : ${x}`)
     console.groupEnd();
 
 
 
 
 
-// // Operadores de Asignacion
-
-// let x;
-// let y= 80;
-// let myStyle = `
-//     color: #000;
-//     background: #7FFF00;
-//     padding: 5px;
-//     border-radius: 5px;
-// `;
-// // Asignacion
-// console.group(`%cOperadores Asignacion`, myStyle);
-
-//     console.log(`
-//         Variable x: ${x}
-//         Variable y: ${y}`);
-
-//     // Abreviacion
-//     x=y;
-//     console.log(`Resultado: x : ${x} y : ${y}`);
-
-// console.groupEnd();
-
-
-
-
-
-
-// // ventana emergente JS
-// let nombre=prompt(`Ingrese el nombre del usuario`,`nombre`);
-
-// console.log(nombre);
