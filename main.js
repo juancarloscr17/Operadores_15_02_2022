661538aa4ab883d4207edc470d1720f254cdfd60
juancarloscr17 <juankcr17@gmail.com>
2022-02-15 20:01:45 -0500

    Operador de Asignacion

--- main.js
+++ main.js
@@ -1,4 +1,32 @@
-// ventana emergente JS
-let nombre=prompt(`Ingrese el nombre del usuario`,`nombre`);
+// Operadores de Asignacion
 
-console.log(nombre);
+let x;
+let y= 80;
+let myStyle = `
+    color: #000;
+    background: #7FFF00;
+    padding: 5px;
+    border-radius: 5px;
+`;
+// Asignacion
+console.group(`%cOperadores Asignacion`, myStyle);
+
+    console.log(`
+        Variable x: ${x}
+        Variable y: ${y}`);
+
+    // Abreviacion
+    x=y;
+    console.log(`Resultado: x : ${x} y : ${y}`);
+
+console.groupEnd();
+
+
+
+
+
+
+// // ventana emergente JS
+// let nombre=prompt(`Ingrese el nombre del usuario`,`nombre`);
+
+// console.log(nombre);
