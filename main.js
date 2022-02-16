18bc7b53a9b14580e6af1e5562b2d286eb02ada6
juancarloscr17 <juankcr17@gmail.com>
2022-02-15 22:26:09 -0500

    Operador Aritmetico Division

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-// Operador Aritmetico Resta
+// Operador Aritmetico Division
 let num1 = parseInt(prompt(`Ingrese el primer numero`, 1));
 let num2 = new Number(prompt(`Ingrese el segundo numero`,2));
 let myStyle = `
     color: 	#fff;
-    background: #ee82ee;
+    background: #8b008b;
     padding: 5px;
     border-radius: 5px;
 `;
-console.group(`%cOperador Aritmetico Resta`, myStyle);
+console.group(`%cOperador Aritmetico Division`, myStyle);
 
  // Significado
-    let resultado = num1 - num2;
-    console.log(`Numero 1: ${num1} - Numero 2: ${num2}`);
-    console.log(`La resta de los numeros es: ${resultado}`);
+    let resultado = num1 / num2;
+    console.log(`Numero 1 / Numero 2 : ${num1} / ${num2} =${resultado}`);
+    console.log(`La Division de los numeros es: ${resultado}`);
 
 // Abreviacion
-    console.log(`Abreviacion: ${num1} -= ${num2}`)
-    num1-=num2
+    console.log(`Abreviacion: ${num1} /= ${num2}`)
+    num1/=num2
     console.log(`La resta es : ${num1}`);
     console.groupEnd();
 
