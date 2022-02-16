5787cd0840aba490172e27b73986ea3503898b75
juancarloscr17 <juankcr17@gmail.com>
2022-02-15 22:31:38 -0500

    Operador Aritmetico Multiplicacion

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-// Operador Aritmetico Division
+// Operador Aritmetico Multiplicacion
 let num1 = parseInt(prompt(`Ingrese el primer numero`, 1));
 let num2 = new Number(prompt(`Ingrese el segundo numero`,2));
 let myStyle = `
     color: 	#fff;
-    background: #8b008b;
+    background: #006400;
     padding: 5px;
     border-radius: 5px;
 `;
-console.group(`%cOperador Aritmetico Division`, myStyle);
+console.group(`%cOperador Aritmetico Multiplicacion`, myStyle);
 
  // Significado
-    let resultado = num1 / num2;
-    console.log(`Numero 1 / Numero 2 : ${num1} / ${num2} =${resultado}`);
-    console.log(`La Division de los numeros es: ${resultado}`);
+    let resultado = num1 * num2;
+    console.log(`Numero 1 X Numero 2 : ${num1} X ${num2} =${resultado}`);
+    console.log(`La Multiplicacion de los numeros es: ${resultado}`);
 
 // Abreviacion
-    console.log(`Abreviacion: ${num1} /= ${num2}`)
-    num1/=num2
-    console.log(`La resta es : ${num1}`);
+    console.log(`Abreviacion: ${num1} *= ${num2}`)
+    num1*=num2
+    console.log(`La Multiplicacion es : ${num1}`);
     console.groupEnd();
 
 
