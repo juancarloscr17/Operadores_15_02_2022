7ad855210a3c5a6d0bb5854ba1b026f72811d09a
juancarloscr17 <juankcr17@gmail.com>
2022-02-15 21:33:30 -0500

    Operadores de Adicion

--- main.js
+++ main.js
@@ -1,25 +1,57 @@
-// Operadores de Asignacion
-
-let x;
-let y= 80;
+// Operadores de Adicion
+let x = 25;
+let y = 10;
 let myStyle = `
     color: #000;
     background: #7FFF00;
     padding: 5px;
     border-radius: 5px;
 `;
-// Asignacion
-console.group(`%cOperadores Asignacion`, myStyle);
 
-    console.log(`
-        Variable x: ${x}
-        Variable y: ${y}`);
+// Adición
+console.group(`%cOperadores Adición`, myStyle);
+
+
+    console.log(`Variable x: ${x} Variable y: ${y}`);
 
     // Abreviacion
-    x=y;
-    console.log(`Resultado: x : ${x} y : ${y}`);
+    console.log(`Abreviacion: x += y;`);
+    x += y;
+    console.log(`Resultado : x : ${x}`)
+    // // Significado
+    // console.log(`Significado: x = x + y;`);
+    // x = x + y;
 
-console.groupEnd();
+    // console.log(`x: ${x} y: ${y}`);
+    // console.log(`Resultado : x : ${x}`)
+    console.groupEnd();
+
+
+
+
+
+// // Operadores de Asignacion
+
+// let x;
+// let y= 80;
+// let myStyle = `
+//     color: #000;
+//     background: #7FFF00;
+//     padding: 5px;
+//     border-radius: 5px;
+// `;
+// // Asignacion
+// console.group(`%cOperadores Asignacion`, myStyle);
+
+//     console.log(`
+//         Variable x: ${x}
+//         Variable y: ${y}`);
+
+//     // Abreviacion
+//     x=y;
+//     console.log(`Resultado: x : ${x} y : ${y}`);
+
+// console.groupEnd();
 
 
 
