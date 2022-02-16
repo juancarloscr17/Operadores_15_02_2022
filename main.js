6643dd38b8cf053fd54325f03f057740a3586906
juancarloscr17 <juankcr17@gmail.com>
2022-02-15 22:17:06 -0500

    Operador Aritmetico Resta

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
-// Operador Aritmetico Suma
+// Operador Aritmetico Resta
 let num1 = parseInt(prompt(`Ingrese el primer numero`, 1));
 let num2 = new Number(prompt(`Ingrese el segundo numero`,2));
 let myStyle = `
-    color: #000;
-    background: #00ffff;
+    color: 	#fff;
+    background: #ee82ee;
     padding: 5px;
     border-radius: 5px;
 `;
-
-// Adición
-console.group(`%cOperadores Aritmetico Suma`, myStyle);
+console.group(`%cOperador Aritmetico Resta`, myStyle);
 
  // Significado
-   
-    let resultado = num1 + num2;
-    console.log(`Numero 1: ${num1} + Numero 2: ${num2}`);
-    console.log(`La suma de los numeros es: ${resultado}`);
+    let resultado = num1 - num2;
+    console.log(`Numero 1: ${num1} - Numero 2: ${num2}`);
+    console.log(`La resta de los numeros es: ${resultado}`);
 
+// Abreviacion
+    console.log(`Abreviacion: ${num1} -= ${num2}`)
+    num1-=num2
+    console.log(`La resta es : ${num1}`);
     console.groupEnd();
 
 
