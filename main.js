107646246334f58eb724cc78f4e31b9a833fd8f3
juancarloscr17 <juankcr17@gmail.com>
2022-02-17 19:18:09 -0500

    Operador Aritmetico Modulo

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-// Operador Aritmetico Exponenciacion
-let num1 = parseInt(prompt(`Ingrese el primer numero`, 1));
-let num2 = new Number(prompt(`Ingrese el segundo numero`,2));
+// Operador Aritmetico Modulo
+let num1 = parseInt(prompt(`Ingrese el valor de X`, 1));
+let num2 = new Number(prompt(`Ingrese el Valor de Y`,2));
 let myStyle = `
-    color: 	#000;
-    background: #ff8c00;
+    color: 	##800080;
+    background: #E9967A;
     padding: 5px;
     border-radius: 5px;
 `;
-console.group(`%cOperador Aritmetico Exponenciacion`, myStyle);
+console.group(`%cOperador Aritmetico Modulo`, myStyle);
 
  // Significado
-    let resultado = num1 ** num2;
-    console.log(`Numero 1 elevado al Numero 2 : ${num1} ** ${num2} =${resultado}`);
-    console.log(`${num1} elevado al ${num2}: ${resultado}`);
+    let resultado = num1 % num2;
+    console.log(`X = X % Y : ${num1} % ${num2} =${resultado}`);
+    console.log(`${num1} modulo ${num2}: ${resultado}`);
 
 // Abreviacion
-    console.log(`Abreviacion: ${num1} **= ${num2}`)
-    num1**=num2
-    console.log(`El resultado del exponencia es : ${num1}`);
+    console.log(`Abreviacion (X %=Y): ${num1} %= ${num2}`)
+    num1%=num2
+    console.log(`El resultado del modulo es : ${num1}`);
     console.groupEnd();
 
 
