f35471961fbce2c19e5531ddc2e3ab5f2b1ecbb5
juancarloscr17 <juankcr17@gmail.com>
2022-02-24 20:38:48 -0500

    Operador de Comparacion Mayor

--- main.js
+++ main.js
@@ -1,16 +1,40 @@
-// Operador de comparacion Diferente
-let x = 5*2;
-//      10 
-let y = (5/5-(3+9) - (5%3)*2);
-//      (5/5-(12)-(5%3)*2) 
-//      (5/5-12- (2)*2)
-//      (1-12-2*2)
-//      1-12-4
-//      -11-4
-//      -15
-let resultado = x != y;
+//Operador de Comparacion Mayor
+let x = 5*(15-(8-1)/9) - (5+9*9)%5;
+//      5*(15-7/9)-(5+81)%5 
+//      5*(15-0.777)-(5+81)%5
+//      5*14.22-(5+81)%5
+//      5*14.22-86%5
+//      71.11 - 86%5
+//      71.11 - 1   
+//      70.11
+let y = (5-2 * 3)+2 * (5-1)+2;
+//       (5-6)+2*(5-1)+2
+//       (-1)+2*(5-1)+2
+//       -1+2*4+2
+//       -1+8+2 
+//       7+2
+//       9
+let resultado = x > y;
 
-console.log(`Variable x: ${x} y: ${y} Diferente '!=' ${resultado}`);
+console.log(`Variable x: ${x} y: ${y} Mayor '>' ${resultado}`);
+
+
+
+
+
+// // Operador de comparacion Diferente
+// let x = 5*2;
+// //      10 
+// let y = (5/5-(3+9) - (5%3)*2);
+// //      (5/5-(12)-(5%3)*2) 
+// //      (5/5-12- (2)*2)
+// //      (1-12-2*2)
+// //      1-12-4
+// //      -11-4
+// //      -15
+// let resultado = x != y;
+
+// console.log(`Variable x: ${x} y: ${y} Diferente '!=' ${resultado}`);
 
 
 
