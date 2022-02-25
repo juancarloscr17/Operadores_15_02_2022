94f7249ba4d52613d88188b8c6ffc4f11e40c525
juancarloscr17 <juankcr17@gmail.com>
2022-02-24 19:58:29 -0500

    Operador de comparacion Diferente

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
-//Operador de Comparacion de  Igualdad
-let x = 5+(2*2)-(6/2);
-//      5+ 4 - 3
-//      9 - 3
-//      6
-let y = 5;
+// Operador de comparacion Diferente
+let x = 5*2;
+//      10 
+let y = (5/5-(3+9) - (5%3)*2);
+//      (5/5-(12)-(5%3)*2) 
+//      (5/5-12- (2)*2)
+//      (1-12-2*2)
+//      1-12-4
+//      -11-4
+//      -15
+let resultado = x != y;
 
-let resultado = x==y;
-console.log(`Variable x: ${x} y: ${y} Igualdad '==' ${resultado}`);
+console.log(`Variable x: ${x} y: ${y} Diferente '!=' ${resultado}`);
+
+
+
+
+
+// //Operador de Comparacion de  Igualdad
+// let x = 5+(2*2)-(6/2);
+// //      5+ 4 - 3
+// //      9 - 3
+// //      6
+// let y = 5;
+
+// let resultado = x==y;
+// console.log(`Variable x: ${x} y: ${y} Igualdad '==' ${resultado}`);
 
 
 
