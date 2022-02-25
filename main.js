6996ed17624e8cbf779184c3c5939ed2a5c4f634
juancarloscr17 <juankcr17@gmail.com>
2022-02-24 19:40:49 -0500

    ejercicio

--- main.js
+++ main.js
@@ -1,24 +1,47 @@
-// Operador Aritmetico Modulo
-let num1 = parseInt(prompt(`Ingrese el valor de X`, 1));
-let num2 = new Number(prompt(`Ingrese el Valor de Y`,2));
-let myStyle = `
-    color: 	##800080;
-    background: #E9967A;
-    padding: 5px;
-    border-radius: 5px;
-`;
-console.group(`%cOperador Aritmetico Modulo`, myStyle);
 
- // Significado
-    let resultado = num1 % num2;
-    console.log(`X = X % Y : ${num1} % ${num2} =${resultado}`);
-    console.log(`${num1} modulo ${num2}: ${resultado}`);
+let reslutado  = 5*((4/4)*(9-7)*2)+6;
+//  5*((1)*(9-2)*2)+6
+//  5*((1))*(2)*2)+6
+//  5*(2*2)+6
+//  5*4+6
+//  20+6
+//  26
+console.log(reslutado);
 
-// Abreviacion
-    console.log(`Abreviacion (X %=Y): ${num1} %= ${num2}`)
-    num1%=num2
-    console.log(`El resultado del modulo es : ${num1}`);
-    console.groupEnd();
+let reslutado2  = 5+1*8+(9 * (5+8) / 2);
+//  5+1*8+(9*(5+8)/2)
+//  5+1*8+(9*(13)/2)
+//  5+1*8+(117/2)
+//  5+1*8+(58.5)
+//  5+ 8 +58.5
+//  71.5
+console.log(reslutado2);
+
+
+
+
+
+// // Operador Aritmetico Modulo
+// let num1 = parseInt(prompt(`Ingrese el valor de X`, 1));
+// let num2 = new Number(prompt(`Ingrese el Valor de Y`,2));
+// let myStyle = `
+//     color: 	##800080;
+//     background: #E9967A;
+//     padding: 5px;
+//     border-radius: 5px;
+// `;
+// console.group(`%cOperador Aritmetico Modulo`, myStyle);
+
+//  // Significado
+//     let resultado = num1 % num2;
+//     console.log(`X = X % Y : ${num1} % ${num2} =${resultado}`);
+//     console.log(`${num1} modulo ${num2}: ${resultado}`);
+
+// // Abreviacion
+//     console.log(`Abreviacion (X %=Y): ${num1} %= ${num2}`)
+//     num1%=num2
+//     console.log(`El resultado del modulo es : ${num1}`);
+//     console.groupEnd();
 
 
 
