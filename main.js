3ddd9a6f2149a5831dd736ce746019fd042a8c3d
juancarloscr17 <juankcr17@gmail.com>
2022-03-03 19:17:54 -0500

    Diseño y operador logico XOR

--- main.js
+++ main.js
@@ -1,20 +1,46 @@
-let a = true;
+let a = false;
 let b = false;
 let validar = function(id, ope){
     document.querySelector(id).insertAdjacentHTML("beforeend",
     `
         <tr>
             <td>${a}</td>
-            <td>${b}</td>
+            ${(ope != "!") ? `<td>${b}</td>` : ""}
             <td>${
                 (ope == "&") ? a && b 
-                : (ope == "|") ? a || b 
+                : (ope == "|") ? a || b                 
+                : (ope == "!") ? !a
                 : ""
             }</td>
+        </tr>
+        <tr>
+            ${(ope != "!") ? `<td>${a}</td><td>${!b}</td>` : `<td>${!a}</td>`}
+            <td>${
+                (ope == "&") ? a && !b 
+                : (ope == "|") ? a || !b                 
+                : (ope == "!") ? !!a
+                : ""
+            }</td>
+        </tr>
+        <tr>
+            ${(ope != "!") ? `<td>${!a}</td><td>${b}</td>` : ""}
+            ${
+                (ope == "&") ? `<td>${!a && b}</td>` 
+                : (ope == "|") ? `<td>${!a || b}</td>`                 
+                : ""
+            }
+        </tr>
+        <tr>
+            ${(ope != "!") ? `<td>${!a}</td><td>${!b}</td>` : ""}
+            ${
+                (ope == "&") ? `<td>${!a && !b}</td>` 
+                : (ope == "|") ? `<td>${!a || !b}</td>`                 
+                : ""
+            }
         </tr>
     `
     );
 };
 validar("#operadorAND", "&");
 validar("#operadorOR", "|");
-validar("operadorNOT","!");
+validar("#operadorNOT","!");
